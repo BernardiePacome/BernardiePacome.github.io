d9836da135e6573ec327e63e7e8d1353865d2621
pbondetdelabernardie <bernardie.pacome@gmail.com>
2024-08-29 16:47:17 +0200

    svelteconfig

--- svelte.config.js
+++ svelte.config.js
@@ -17,7 +17,7 @@ const config = {
     paths: {
       base:
         process.env.NODE_ENV === "production"
-          ? "BernardiePacome.github.io/"
+          ? "/BernardiePacome.github.io"
           : "",
     },
   },
